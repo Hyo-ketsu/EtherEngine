28a5880ba87bc897f26854829698090d395558b7
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-03-30 19:06:32 +0900

    IMGUI,Eigenの導入

--- EtherEngine/Source/Base/stdafx/BaseStdafx.h
+++ EtherEngine/Source/Base/stdafx/BaseStdafx.h
@@ -4,3 +4,11 @@
 #include <mutex>
 #include <vector>
 #include <thread>
+#include <memory>
+#include <mutex>
+#include <functional>
+#include <atomic>
+#include <condition_variable>
+
+#include <Base/IMGUI/imgui.h>
+#include <Eigen/Core>
