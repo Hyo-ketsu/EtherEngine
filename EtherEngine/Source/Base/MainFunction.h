8d8483caa337c4eb570b57dcee7ccafc961a2d58
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-04-10 17:51:18 +0900

    Main関数の追加

--- EtherEngine/Source/Base/MainFunction.h
+++ EtherEngine/Source/Base/MainFunction.h
@@ -2,12 +2,14 @@
 #define I_MAIN_FUNCTION_H
 
 
-// メイン関数用インターフェース
-class IMainFunction {
-public:
-	// メイン関数
-	virtual void Main(void) = 0;
-};
+namespace EtherEngine {
+    // メイン関数用インターフェース
+    class IMainFunction {
+    public:
+        // メイン関数
+        virtual void Main(void) = 0;
+    };
+}
 
 
 #endif // !I_MAIN_FUNCTION_H
--- EtherEngine/Source/EtherEngine/EditorMain.cpp
+++ EtherEngine/Source/EtherEngine/EditorMain.cpp
@@ -1,3 +1,6 @@
+#include <EtherEngine/EditorApplication.h>
+
+
 //----- DirectXƒƒCƒ“ŠÖ”
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine, int nCmdShow) {
 }
