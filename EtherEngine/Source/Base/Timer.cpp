18e26406e3f60349ef803dd7350707324fed27c3
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-04-10 17:09:04 +0900

    Timer追加

---
+++ EtherEngine/Source/Base/Timer.cpp
@@ -0,0 +1,49 @@
+#include <Base/Timer.h>
+
+using namespace EtherEngine;
+
+
+// 現在時間を取得する
+// @ Ret  :  現在時刻
+std::chrono::milliseconds Timer::GetTime(void) {
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(GlobalTimer::Get()->GetGlobalTime());
+}
+
+
+// 前回からの経過時間を取得する
+std::chrono::milliseconds Timer::GetDeltaTime(void) {
+    //----- usingと変数宣言
+    using namespace std::chrono;
+    std::chrono::milliseconds ret{ 0ms };
+
+    //----- 減算処理
+    if (m_deltaTime.has_value()) {
+        //----- 有効値。処理する
+        ret = duration_cast<milliseconds>(m_deltaTime.value() - std::chrono::steady_clock::now());
+    }
+
+    //----- 時間更新
+    m_deltaTime.value() = std::chrono::steady_clock::now();
+
+    //----- 返却
+    return ret;
+}
+
+
+
+// 初期化
+void GlobalTimer::Init(void) {
+    m_firstTime = std::chrono::steady_clock::now();
+}
+// 終了
+void GlobalTimer::Uninit(void) {
+
+}
+
+
+// グローバルタイムを取得する
+// @ Ret  : GlobalTimer初期時からの経過時間
+std::chrono::nanoseconds GlobalTimer::GetGlobalTime(void) {
+    return std::chrono::steady_clock::now() - m_firstTime.value();
+}
---
+++ EtherEngine/Source/Base/Timer.h
@@ -0,0 +1,56 @@
+#ifndef I_TIMERBASE_H
+#define I_TIMERBASE_H
+
+#include <Base/Singleton.h>
+
+
+namespace EtherEngine {
+    // タイマー
+    class Timer {
+    public:
+        // 現在時間を取得する
+        // @ Ret  :  現在時刻
+        std::chrono::milliseconds GetTime(void);
+
+
+        // 前回からの経過時間を取得する
+        // @ Memo : 初使用の際は0を返却します
+        // @ Ret  : 前回DeltaTime使用時からの経過時間
+        std::chrono::milliseconds GetDeltaTime(void);
+
+    private:
+        std::optional<std::chrono::steady_clock::time_point> m_deltaTime;   // 前回DeltaTime使用時時刻
+    };
+
+
+    // グローバルタイマー
+    class GlobalTimer : public Singleton<GlobalTimer> {
+    public:
+        // デストラクタ
+        ~GlobalTimer(void) override {}
+
+
+        // 初期化
+        // @ Memo : この初期化を行った時間を起点にします
+        void Init(void);
+        // 終了
+        void Uninit(void);
+
+
+        // グローバルタイムを取得する
+        // @ Ret  : GlobalTimer初期時からの経過時間
+        std::chrono::nanoseconds GetGlobalTime(void);
+
+    private:
+        // コンストラクタ
+        GlobalTimer(void) {}
+
+
+        std::optional<std::chrono::steady_clock::time_point> m_firstTime;   // 初期化時刻
+
+        friend class Singleton<GlobalTimer>;
+    };
+}
+
+
+#endif // !I_TIMER_H
