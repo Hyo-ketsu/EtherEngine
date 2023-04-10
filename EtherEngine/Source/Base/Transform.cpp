68f06c3a31842b152518e1b1f0c9e5aa0706fd6b
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-04-10 17:47:37 +0900

    座標・拡縮・回転を管理するクラス、Transformの追加

--- EtherEngine/Source/Base/Transform.cpp
+++ EtherEngine/Source/Base/Transform.cpp
@@ -1,12 +1,14 @@
 #include <Base/Transform.h>
 
+using namespace EtherEngine;
+
 
 // コンストラクタ
 // @ Arg1 : 座標(デフォルト：全て0)
 // @ Arg2 : 拡縮(デフォルト：全て0)
 // @ Arg3 : 回転(デフォルト：全て1)
 Transform::Transform(Eigen::Vector3f pos, Eigen::Vector3f sca, Eigen::Vector3f rot) 
-	: m_postion(pos)
-	, m_scale(sca)
-	, m_rotation(rot) {
+    : m_postion(pos)
+    , m_scale(sca)
+    , m_rotation(rot) {
 }
