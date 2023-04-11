5b1ae6938a88927b8a5296ecd150f8b89243debc
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-04-11 21:12:11 +0900

    プリコンパイルヘッダのインクルードを追加・整理。

--- EtherEngine/Source/Base/stdafx/BaseStdafx.h
+++ EtherEngine/Source/Base/stdafx/BaseStdafx.h
@@ -1,25 +1,30 @@
 #include <array>
-#include <list>
-#include <memory>
-#include <mutex>
-#include <vector>
-#include <thread>
-#include <memory>
-#include <mutex>
-#include <functional>
 #include <atomic>
-#include <condition_variable>
-#include <type_traits>
-#include <optional>
-#include <deque>
 #include <chrono>
+#include <condition_variable>
+#include <float.h> 
+#include <functional>
+#include <deque>
+#include <limits.h>
+#include <list>
+#include <math.h>  
+#include <memory>
+#include <mutex>
+#include <optional>
 #include <random>
+#include <vector>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <string.h>
+#include <thread>
+#include <type_traits>
 #include <unordered_map>
 
-
-#include <Base/IMGUI/imgui.h>
 #include <Base/Json/nlohmann/json.hpp>
 #include <Eigen/Core>
+
 
 namespace EtherEngine {
     constexpr float ONE_FRAME = 1.f / 60.f;
