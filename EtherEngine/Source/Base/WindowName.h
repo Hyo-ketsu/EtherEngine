a5a8b15a97fac1c57cf1e931f7041695d73d696a
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-05-23 15:04:10 +0900

    ウィンドウ名定数追加

---
+++ EtherEngine/Source/Base/WindowName.h
@@ -0,0 +1,13 @@
+#ifndef I_WINDOWNAME_H
+#define I_WINDOWNAME_H
+
+
+namespace EtherEngine {
+    namespace WindowName {
+        const std::string GAME_APPLICATION_NAME = "Game";
+        const std::string EDITOR_NAME = "EtherEgnine";
+    }
+}
+
+
+#endif // !I_WINDOWNAME_H
