d051c7e6f523f2f6d1414409fd1e28a7cac9b155
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2023-09-26 09:25:56 +0900

    プログレスバー作成中 EditorWindowBaseブランチでやっていたのを忘れていたのでマージ

--- EtherEngine/Source/EtherEngine/EditorProgressBar.h
+++ EtherEngine/Source/EtherEngine/EditorProgressBar.h
@@ -1,11 +1,25 @@
 #ifndef I_EDITORPROGRESSBAR_H
 #define I_EDITORPROGRESSBAR_H
-#include <EtherEngine/EditorWindowBase.h>
+#include <EtherEngine/EditorPopupWindow.h>
 
 
 //----- EditorProgressBar 定義
 namespace EtherEngine {
-    class EditorProgressBar : public EditorWindowBase {
+    // プログレスバーを表現するクラス
+    class ProgressClass {
+    public:
+        // コンストラクタ
+    };
+
+
+    // プログレスバーを表示するポップアップウィンドウ
+    class EditorProgressBar : public EditorPopupWindow {
+    public:
+        // コンストラクタ　
+        // @ Arg1 : エディターオブジェクト
+        // @ Arg2 : 表示メッセージ
+        // @ Arg3 : 
+        EditorProgressBar()
 
     };
 }
