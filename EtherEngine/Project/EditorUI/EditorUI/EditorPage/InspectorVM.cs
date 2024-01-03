767c164e96c8d8807c75121b80ce9e97809d5a71
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2024-01-03 13:19:57 +0900

    Inspector改修中

--- EtherEngine/Project/EditorUI/EditorUI/EditorPage/InspectorVM.cs
+++ EtherEngine/Project/EditorUI/EditorUI/EditorPage/InspectorVM.cs
@@ -4,11 +4,17 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Reactive.Bindings;
+using Reactive.Bindings.Interactivity;
 
 
 namespace EditorUI {
     /// <summary>ViewModel</summary>
     internal class InspectorVM : INotifyPropertyChanged {
         public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        /// <summary>現在参照しているオブジェクト</summary>
+        public static ReactiveProperty<EtherEngine.BaseObject> InspectoObject { get; set; } = new();
     }
 }
--- EtherEngine/Project/EditorUI/EditorUI/EditorPage/InspectorWindow.xaml.cs
+++ EtherEngine/Project/EditorUI/EditorUI/EditorPage/InspectorWindow.xaml.cs
@@ -23,6 +23,51 @@ namespace EditorUI {
     public partial class InspectorWindow : UserControl {
         public InspectorWindow() {
             InitializeComponent();
+
+            InspectorVM.InspectoObject.Subscribe(baseObject => {
+                if (baseObject != null) {
+                    CreateInspectorItem(baseObject);
+                }
+                else {
+                    Inspector.Children.Clear();
+                }
+            });
+        }
+
+
+        /// <summary>インスペクター用のUIを生成する</summary>
+        public void CreateInspectorItem(EtherEngine.BaseObject baseObject) {
+            //----- まずはClearする
+            Inspector.Children.Clear();
+
+            //----- 型の情報取得
+            var classDatas = EtherEngine.ClassLoader.GetClassData(baseObject, typeof(EtherEngine.BaseObject));
+
+            //----- それぞれの型に対応したコントロールを追加
+            foreach (var classData in classDatas) {
+                //----- それぞれの型に対応したコントロール表示
+                if (classData.FieldType == typeof(EtherEngine.BaseObject)) {
+                    var objectReference = new EditorUI.ObjectReference();
+                    objectReference.Object = classData.GetValue(baseObject) as EtherEngine.BaseObject;
+                    Inspector.Children.Add(objectReference);
+
+                    continue;
+                }
+                if (classData.FieldType == typeof(int) || classData.FieldType == typeof(short) || classData.FieldType == typeof(long) ||
+                    classData.FieldType == typeof(uint) || classData.FieldType == typeof(ushort) || classData.FieldType == typeof(ulong) ||
+                    classData.FieldType == typeof(float)) {
+                    var numberInput = new EditorUI.NumberInput();
+                    numberInput.NumberType = classData.FieldType;
+                    // @ MEMO : 後回し
+                    //numberInput.SetValue(classData.GetValue(baseObject) as );
+                    Inspector.Children.Add(numberInput);
+                }
+                if (classData.FieldType == typeof(string)) {
+                    var textBox = new TextBox();
+                    textBox.Text = classData.GetValue(baseObject) as string;
+                    Inspector.Children.Add(textBox);
+                }
+            }
         }
     }
 }
