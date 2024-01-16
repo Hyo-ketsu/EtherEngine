af191c22deff78779176b960d67ef535e21a28cb
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2024-01-16 13:29:37 +0900

    翻訳テキスト表示コントロール、言語設定ボタン等Controlに分割

---
+++ EtherEngine/Project/EditorUI/EditorUI/Control/EditorTextControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Reactive.Bindings;
+
+
+namespace EditorUI {
+    /// <summary>
+    /// EditorTextControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class EditorTextControl : UserControl {
+        public EditorTextControl() {
+            //----- 初期化
+            InitializeComponent();
+
+            //----- 文字列設定
+            EditorSetting.Get.Language.Subscribe(language => {
+                Text.Value = EditorText.ToString();
+            });
+
+            //----- DataContextへ追加
+            DataContext = this;
+        }
+
+
+        public ReactiveProperty<string> Text { get; private set; } = new();
+        public string EN { set { 
+                EditorText.EN = value; 
+                Text.Value = EditorText.ToString();
+            }
+        }
+        public string JP { set { 
+                EditorText.JP = value;
+                Text.Value = EditorText.ToString();
+            } 
+        }
+
+        
+        public EditorText EditorText { get; private set; } = new();
+    }
+}
---
+++ EtherEngine/Project/EditorUI/EditorUI/Control/LanguageButton.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace EditorUI {
+    /// <summary>View Model</summary>
+    public class LanguageButtonVM : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        /// <summary>コンストラクタ</summary>
+        public LanguageButtonVM() {
+            Language.SelectValue.Value = EditorSetting.Get.Language.Value;
+            Language.SelectValue.Subscribe(language => {
+                EditorSetting.Get.Language.Value = language;
+            });
+        }
+
+
+        /// <summary>言語設定</summary>
+        public EnumViewModel<EditorLanguageEnum> Language { get; private set; } = new();
+    }
+
+
+    /// <summary>
+    /// LanguageButton.xaml の相互作用ロジック
+    /// </summary>
+    public partial class LanguageButton : UserControl {
+        public LanguageButton() {
+            InitializeComponent();
+        }
+    }
+}
