0e26b297e07e5cdff47ab8ecdcd56f35436494f5
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2024-02-09 10:15:22 +0900

    コントロール追加(SeparateLabelContentsは表示の更新ができません。用改修)

---
+++ EtherEngine/Project/EditorUI/EditorUI/Control/SeparateLabelContents.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Reactive.Bindings;
+
+
+namespace EditorUI {
+    /// <summary>
+    /// SeparateLabelContents.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SeparateLabelContents : UserControl {
+        public SeparateLabelContents() {
+            InitializeComponent();
+        }
+
+
+        /// <summary>ラベルの表示名</summary>
+        public string Text { 
+            set {
+                var label = new Label();
+                label.Content = value;
+                LabelGrid.Children.Clear();
+                LabelGrid.Children.Add(label);
+            }
+        }
+        /// <summary>ラベルの表示物</summary>
+        public UIElement LabelContents {
+            set {
+                LabelGrid.Children.Clear();
+                LabelGrid.Children.Add(value);
+            }
+        }
+
+
+        /// <summary>表示するコンテンツ</summary>
+        public UIElement Contents {
+            set {
+                ContentsGrid.Children.Clear();
+                ContentsGrid.Children.Add(value); 
+            }
+        }
+    }
+}
