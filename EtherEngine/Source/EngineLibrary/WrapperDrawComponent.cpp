e216c120cb26391b63fc7556ec19baf99def9cb3
Fukaya Yuuki <101307356+Hyo-ketsu@users.noreply.github.com>
2024-02-26 21:28:59 +0900

    ひとまず中身消去

--- EtherEngine/Source/EngineLibrary/WrapperDrawComponent.cpp
+++ EtherEngine/Source/EngineLibrary/WrapperDrawComponent.cpp
@@ -4,43 +4,4 @@
 
 //----- WrapperDrawComponent 定義
 namespace EtherEngine {
-    // コンストラクタ
-    WrapperDrawComponent::WrapperDrawComponent(NativeGameObject* gameObject, const std::string& componentName)
-        : WrapperComponent(gameObject, componentName) 
-        , NativeDrawComponent(gameObject, componentName) {
-    }
-    // 開始処理
-    void WrapperDrawComponent::Start(void) {
-        WrapperComponent::Start();
-    }
-    // 更新処理
-    void WrapperDrawComponent::Update(void) {
-        WrapperComponent::Update();
-    }
-    // 削除時処理
-    void WrapperDrawComponent::Delete(void) {
-        WrapperComponent::Delete();
-    }
-    // 衝突開始処理
-    void WrapperDrawComponent::CollisionStart(void) {
-        WrapperComponent::CollisionStart();
-    }
-    // 衝突終了処理
-    void WrapperDrawComponent::CollisionEnd(void) {
-        WrapperComponent::CollisionEnd();
-    }
-    // 衝突処理
-    void WrapperDrawComponent::CollisionHit(void) {
-        WrapperComponent::CollisionHit();
-    }
-    // 描画
-    void WrapperDrawComponent::Draw(void) {    
-        //----- キャスト
-        auto userBaseComponent = safe_cast<Component^>(m_component);
-        auto userDrawBaseComponent = dynamic_cast<DrawComponent^>(userBaseComponent);
-
-        //----- 描画
-        userDrawBaseComponent->Bind();
-        userDrawBaseComponent->Draw();
-    }
 }
--- EtherEngine/Source/EngineLibrary/WrapperDrawComponent.h
+++ EtherEngine/Source/EngineLibrary/WrapperDrawComponent.h
@@ -4,28 +4,4 @@
 #include <EngineLibrary/WrapperDrawComponent.h>
 
 
-//----- 描画コンポーネント
-namespace EtherEngine {
-    // 描画コンポーネント
-    public ref class WrapperDrawComponent : public Component {
-    public:
-        // コンストラクタ
-        // @ Memo : コンポーネントが生成できなければ例外を送出します
-        // @ Arg1 : 親ゲームオブジェクト
-        // @ Arg2 : 生成コンポーネント名
-        WrapperDrawComponent(NativeGameObject* gameObject, const std::string& componentName);
-
-
-        // 描画前更新処理
-        void Update(void) override;
-        // 描画後更新処理
-        void LateUpdate(void) override;
-
-
-        // 描画
-        void Draw(void) override;
-    };
-}
-
-
 #endif // !I_GAMEDRAWCOMPONENT_H
